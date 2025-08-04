964c4c48f89d3ac99892c4b93532165881214ac9
ederlucascavalcante-cell <ederlucascavalcante@gmail.com>
2025-08-04 15:13:38 -0300

    Ajustando a lógica do jogo, para jogador 1

--- TesteJogo.py
+++ TesteJogo.py
@@ -2,13 +2,14 @@ import random
 import os
 import platform
 
-#Só pra ver
 
 matriz_memoria = []
+matriz_memoria_bool = []
 jogador_um = True
 jogador_dois = False
 escolha1,escolha2 = [],[]
 l1, c1, l2, c2 = 0,0,0,0
+lista_sigilo = [['xxxx','xxxx','xxxx','xxxx'],['xxxx','xxxx','xxxx','xxxx'],['xxxx','xxxx','xxxx','xxxx']]
 
 def limpar_terminal():
     sistema_operacional = platform.system()
@@ -18,14 +19,26 @@ def limpar_terminal():
     else:
         os.system("clear")
 
-def esconder_cartao(n):
-    #matriz que aparecerá para ninguém ver nada
-    lista_sigilo = [['xxxx','xxxx','xxxx','xxxx'],['xxxx','xxxx','xxxx','xxxx'],['xxxx','xxxx','xxxx','xxxx']]
+def esconder_cartao():
     for i in range(3):
         print(lista_sigilo[i])
-        print()
     print()
 
+
+def mostrar_cartao(l,c,cor):
+    lista_sigilo[l][c] = cor
+    for i in range(3):
+        print(lista_sigilo[i])
+    print()
+    lista_sigilo[l][c] = 'xxxx'
+
+def atualizar_tabuleiro(l1,c1,cor1, l2, c2, cor2):
+    lista_sigilo[l1][c1] = cor1
+    lista_sigilo[l2][c2] = cor2
+
+def atualizar_tabuleiro_bool(l1,c1, l2, c2):
+    matriz_memoria_bool[l1][c1] = True
+    matriz_memoria_bool[l2][c2] = True
 
 def found_colors (n):
     if n == 1:
@@ -54,10 +67,13 @@ lista_num_cores = [1,1,2,2,3,3,4,4,5,5,6,6]
 
 for i in range (3):
     linha = []
+    linha_bool = []
     for j in range(4):
         numero_da_vez = random.choice(lista_num_cores)
+        linha_bool += [False]
         linha += [numero_da_vez]
         lista_num_cores.remove(numero_da_vez)
+    matriz_memoria_bool += [linha_bool]
     matriz_memoria += [linha]
 ## aqui é selecionado um número entre os da matriz
 ## e esse mesmo número é retirado da lista inicial para que não possa ser escolhido novamente
@@ -75,27 +91,36 @@ for i in range(3):
 while True:
 
 
-    esconder_cartao(0)
+    esconder_cartao()
 
 
     if jogador_um:
-        escolha1 = input().split()
-        l1 = int(escolha1[0])
-        c1 = int(escolha1[1])
-        print(lista_cores_reais[l1][c1])
-        print()
-
-
-        escolha2 = input().split()
-        l2 = int(escolha2[0])
-        c2 = int(escolha2[1])
-        print(lista_cores_reais[l2][c2])
-        print()
-
+        while True:
+            escolha1 = input().split()
+            l1 = int(escolha1[0])
+            c1 = int(escolha1[1])
+            if matriz_memoria_bool[l1][c1] == False:
+                print(mostrar_cartao(l1, c1, lista_cores_reais[l1][c1]))
+                print()
+                break
+            else:
+                print("Essa carta ja foi encontrada")
+            
+        while True:
+            escolha2 = input().split()
+            l2 = int(escolha2[0])
+            c2 = int(escolha2[1])
+            if matriz_memoria_bool[l2][c2] == False:
+                print(mostrar_cartao(l2, c2, lista_cores_reais[l2][c2]))
+                print()
+                break
+            else:
+                print("Essa carta ja foi encontrada")
 
         limpar_terminal()
 
-
         if apelido[l1][c1] == apelido[l2][c2]:
             print("You find one pair! Congratulations!")
-            break
+            atualizar_tabuleiro(l1, c1, lista_cores_reais[l1][c1], l2, c2, lista_cores_reais[l2][c2])
+            atualizar_tabuleiro_bool(l1,c1, l2,c2)
+            print(esconder_cartao())
