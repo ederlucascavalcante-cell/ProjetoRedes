d09b076520a74208aaebae4ab44baf84cd7cc8ea
ederlucascavalcante-cell <ederlucascavalcante@gmail.com>
2025-08-14 22:15:33 -0300

    Delete tempCodeRunnerFile.py
    Sequer sei de onde veio código

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-primeira_escolha_btn = None
-segunda_escolha_btn = None
-cartoes_encontrados = 0
-botoes = []  # Lista para armazenar os objetos Button
-is_cheking = False  # Flag para evitar cliques enquanto o par está sendo verificado
-
-frame_tabuleiro, janela_principal, matriz_logica = iniciar()
